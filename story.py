f4727c9604dc70dad3a47d9b97aa588a81c96f3c
Kamal Alnakhala <232884661+kalnakha-cmyk@users.noreply.github.com>
2025-10-16 23:00:32 +0000

    Added a center path.
    clear

--- story.py
+++ story.py
@@ -5,14 +5,23 @@ def intro():
         left_path()
     elif choice == "right":
         right_path()
+    elif choice == "ceneter":
+        ceneter_path()    
     else:
         print("You stand still, unsure what to do. The forest swallows you.")
 
 def left_path():
     print("You walk left and find a mysterious glowing sword stuck in a stone.")
+    print("The hero steps onto the left path.")
+    print("A fierce dragon appears!")
+def ceneter_path():
+    print("You walk ceneter and find a mysterious glowing sword stuck in a stone.")
 
 def right_path():
     print("You walk right and encounter a talking squirrel who challenges you to a duel.")
+    print("The Hero takes the right path.")
+    print("A dark curse overtakes them.") 
+    0
 
 if __name__ == "__main__":
     intro()
